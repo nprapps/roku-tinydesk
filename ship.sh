fce6aa50013976744429a7f8088f285efabde7b8
Christopher Groskopf <staringmonkey@gmail.com>
2013-04-03 15:20:37 -0500

    Tweak ship script.

--- ship.sh
+++ ship.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 APP_NAME="roku-tinydesk"
-ZIP_NAME="roku-tinydesk.zip"
 VERSION="$1"
+PKG_NAME="$APP_NAME-$VERSION"
 
 time=" `date +%s`"
 
-output="`curl -s -S -F "mysubmit=Package" -F "app_name=$APP_NAME-$VERSION" -F "passwd=$ROKU_PASSWORD" -F "pkg_time=" http://$ROKU_IP/plugin_package`"
+output="`curl -s -S -F "mysubmit=Package" -F "app_name=$PKG_NAME" -F "passwd=$ROKU_PASSWORD" -F "pkg_time=" http://$ROKU_IP/plugin_package`"
 
 path="`echo $output | sed 's/.*\(pkgs\/.*\.pkg\).*/\1/'`"
 
-curl --output "$APP_NAME-$VERSION.zip" http://$ROKU_IP/$path
+curl --output "$PKG_NAME.zip" http://$ROKU_IP/$path
 
+echo "Created $PKG_NAME.zip"
