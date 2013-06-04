ec6377f9a9a076e652c6962555c67262794d3bbe
Christopher Groskopf <staringmonkey@gmail.com>
2013-06-04 08:47:36 -0500

    Fix binary size. Closes #147.

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ ZIP_NAME="roku-tinydesk.zip"
 ./uninstall.sh
 
 rm $ZIP_NAME;
-zip $ZIP_NAME -r -9 . -x ".git/*"
+zip $ZIP_NAME -r -9 . -i "source/*" -i "images/*" -i "manifest"
 
 curl -s -S -F "mysubmit=Install" -F "archive=@$ZIP_NAME" -F "passwd=" http://$ROKU_IP/plugin_install
